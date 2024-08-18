6b6ce0d30bb60dda16819ba979a4939e3a50b4cf
zaindhedhi <zaindhedhi@oulook.com>
2024-08-18 15:56:19 +0500

    initialize

---
+++ app.js
@@ -0,0 +1,12 @@
+// const image = document.querySelector('#img');
+
+// img.addEventListener('mouseover' , function (){
+//     image.src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtftDEvXLJ_rDUsdsVEwidChScRr4EAduDLQ&s'
+// })
+
+// img.addEventListener('mouseout' , function(){
+//     image.src = 'https://toppng.com/uploads/preview/light-bulb-on-off-png-115539402943y50vxr5yi.png'
+// })
+
+const form = document.querySelector('#form');
+const form = document.querySelector('#form');
