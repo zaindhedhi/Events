7906d5c2dfcd806fdc2db8f45609882b45f9c658
zaindhedhi <zaindhedhi@oulook.com>
2024-09-15 18:50:04 +0500

    add

--- app.js
+++ app.js
@@ -8,5 +8,19 @@
 //     image.src = 'https://toppng.com/uploads/preview/light-bulb-on-off-png-115539402943y50vxr5yi.png'
 // })
 
-const form = document.querySelector('#form');
-const form = document.querySelector('#form');
+// const form = document.querySelector('#form');
+// const form = document.querySelector('#form');
+
+
+// bubbling imageclick phele or ul click bad 
+// capturing ulclick phele or image bad
+const ul = document.querySelector('ul');
+const image = document.querySelector('#image-2')
+
+ul.addEventListener('click' , function(event){
+    console.log(event.target.parentNode.remove())
+} , false)
+
+image.addEventListener('click' , function(){
+    // console.log('image click')
+} , false)
